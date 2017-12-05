285f958741039bf4fa96581161903f7d9534809f
Dominik Süß <dominik.suess@outlook.at>
2017-12-05 16:55:56 +0100

    Added simple webhook

--- node/node.go
+++ node/node.go
@@ -4,16 +4,19 @@ import (
 	"container/list"
 	"encoding/base64"
 	"errors"
+	"io"
+	"net"
+	"net/http"
+	"net/url"
+	"strconv"
+	"time"
+
 	log "github.com/sirupsen/logrus"
 	"github.com/u-speak/core/chain"
 	"github.com/u-speak/core/config"
 	d "github.com/u-speak/core/node/protoc"
 	context "golang.org/x/net/context"
 	"google.golang.org/grpc"
-	"io"
-	"net"
-	"strconv"
-	"time"
 )
 
 const (
@@ -29,6 +32,9 @@ type Node struct {
 	ListenInterface  string
 	Version          string
 	remoteInterfaces map[string]struct{}
+	Hooks            struct {
+		PreAdd string
+	}
 }
 
 type ChainStatus struct {
@@ -77,6 +83,7 @@ func New(c config.Configuration) (*Node, error) {
 		PostChain:        pc,
 		Version:          c.Version,
 		remoteInterfaces: make(map[string]struct{}),
+		Hooks:            c.Hooks,
 	}, nil
 }
 
@@ -252,6 +259,21 @@ func (n *Node) AddBlock(ctx context.Context, block *d.Block) (*d.PushReturn, err
 		}
 
 	}
+	// PreAdd hook
+	if n.Hooks.PreAdd != "" {
+		u, err := url.Parse(n.Hooks.PreAdd)
+		if err != nil {
+			log.Errorf("Error running PreAdd hook: %s", err.Error())
+		}
+		q := u.Query()
+		q.Add("hash", base64.URLEncoding.EncodeToString(b.Hash().Bytes()))
+		u.RawQuery = q.Encode()
+		log.Debugf("Calling PreAdd Hook with URL: %s", u.String())
+		_, err = http.Get(u.String())
+		if err != nil {
+			log.Errorf("Error running PreAdd hook: %s", err.Error())
+		}
+	}
 	n.SmartAdd(b)
 	return &d.PushReturn{}, nil
 }
@@ -376,4 +398,7 @@ func dial(r string) (*grpc.ClientConn, error) {
 			grpc.MaxCallRecvMsgSize(MaxMsgSize),
 			grpc.MaxCallSendMsgSize(MaxMsgSize),
 		))
+}
+
+func runHook(s string) {
 }
