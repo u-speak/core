19f941617b2e2fbb3d419f49be897918a840c5da
Dominik Süß <dominik.suess@outlook.at>
2017-12-06 17:43:03 +0100

    Added more tests

--- api/api.go
+++ api/api.go
@@ -3,12 +3,10 @@ package api
 import (
 	"bytes"
 	"encoding/base64"
-	"errors"
 	"image"
 	"io"
 	"net/http"
 	"strconv"
-	"strings"
 	"time"
 
 	"image/jpeg"
@@ -20,7 +18,6 @@ import (
 	"github.com/u-speak/core/chain"
 	"github.com/u-speak/core/config"
 	"github.com/u-speak/core/node"
-	"github.com/u-speak/core/util"
 	"github.com/u-speak/logrusmiddleware"
 )
 
@@ -36,6 +33,7 @@ type API struct {
 	password        string
 }
 
+// Error is returned when something has gone wrong
 type Error struct {
 	Message string `json:"message"`
 	Code    int    `json:"code"`
@@ -331,65 +329,4 @@ func (a *API) addNode(c echo.Context) error {
 		})
 	}
 	return c.NoContent(http.StatusOK)
-}
-
-func jsonize(b *chain.Block) jsonBlock {
-	h := b.Hash()
-	p := b.PrevHash
-	return jsonBlock{
-		Hash:         base64.URLEncoding.EncodeToString(h[:]),
-		Nonce:        b.Nonce,
-		PrevHash:     base64.URLEncoding.EncodeToString(p[:]),
-		Content:      b.Content,
-		Signature:    b.Signature,
-		Type:         b.Type,
-		PubKey:       b.PubKey,
-		Date:         b.Date.Unix(),
-		BubbleBabble: util.EncodeBubbleBabble(h),
-	}
-}
-
-func decodeImageHash(s string) (chain.Hash, string) {
-	a := strings.Split(s, ".")
-	h, _ := decodeHash(a[0])
-	if len(a) == 1 {
-		return h, ""
-	}
-	switch a[1] {
-	case "png":
-		return h, "image/png"
-	case "jpg", "jpeg":
-		return h, "image/jpeg"
-	}
-	return h, ""
-}
-
-func decodeHash(s string) (chain.Hash, error) {
-	h := [32]byte{}
-	var hs []byte
-	h, err := util.DecodeBubbleBabble(s)
-	if err == nil {
-		return h, nil
-	}
-	hs, err = base64.URLEncoding.DecodeString(s)
-	if err == nil {
-		copy(h[:], hs)
-		return h, nil
-	}
-	hs, err = base64.StdEncoding.DecodeString(s)
-	if err == nil {
-		copy(h[:], hs)
-		return h, nil
-	}
-	hs, err = base64.RawURLEncoding.DecodeString(s)
-	if err == nil {
-		copy(h[:], hs)
-		return h, nil
-	}
-	hs, err = base64.RawStdEncoding.DecodeString(s)
-	if err == nil {
-		copy(h[:], hs)
-		return h, nil
-	}
-	return [32]byte{}, errors.New("Could not parse base64 data")
 }
